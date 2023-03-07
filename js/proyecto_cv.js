19e132a6e2eb5acc0ac19483f9a74626e5bcc7d3
Luis Hirschfeld <Luis.hirschfeld@hotmail.com>
2023-03-07 01:27:15 -0300

    agregué cositas en JS y CSS, elementos hidden

--- js/proyecto_cv.js
+++ js/proyecto_cv.js
@@ -1,11 +1,8 @@
 
-<script>
-function myFunction() {
-  var x = document.createElement("BUTTON");
-  var t = document.createTextNode("Click me");
-  x.appendChild(t);
-  document.body.appendChild(x);
-}
-</script>
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-42942064-1', 'randomuser.me');ga('send', 'pageview');
+const element = document.getElementById("botonl1");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("l1").style.visibility = "visible";
+}
