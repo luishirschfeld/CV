01db7cd70fc90d9f1f1893635caa374e9af1e1f2
Luis Hirschfeld <Luis.hirschfeld@hotmail.com>
2023-03-07 19:34:22 -0300

    Agregado el JS para que muestre la información al pulsar ver más

--- js/proyecto_cv.js
+++ js/proyecto_cv.js
@@ -1,8 +1,8 @@
 
-
 const element = document.getElementById("botonl1");
 element.addEventListener("click", myFunction);
 
 function myFunction() {
   document.getElementById("l1").style.visibility = "visible";
 }
+const columnas = document.querySelectorAll("columna")
